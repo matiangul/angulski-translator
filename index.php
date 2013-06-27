3b6755384fce4a5c106d8c494fbfc1c7ec049d4f
matiangul <mateusz.angulski@gmail.com>
2013-06-27 20:36:35 +0200

    only googe, toggling keyboard, no inputs - handling key events

--- index.php
+++ index.php
@@ -20,13 +20,13 @@
 require 'cgi/FrenglyService.php';
 require 'cgi/GoogleTranslateService.php';
 //strings
-$pl_de = 'Polski -> Niemiecki';
-$de_pl = 'Niemiecki -> Polski';
+$pl_de = 'Polski ~> Niemiecki';
+$de_pl = 'Niemiecki ~> Polski';
 $translate = 'Tłumacz';
 $exception = 'Twój tłumacz nie ma teraz czasu';
 //process request
 $defaultLanguage = 'pl_de';
-$defaultTranslation = '...';
+$defaultTranslation = '';
 $defaultText = '';
 $language = ( isset( $_POST['language-dir'] ) == true ) ? $_POST['language-dir'] : $defaultLanguage;
 $text = ( isset( $_POST['text'] ) == true ) ? $_POST['text'] : $defaultText;
@@ -37,19 +37,6 @@ if ( isset( $_POST['language-dir'] ) && isset( $_POST['text'] ) ) {
 	$languages = explode( '_', $language );
 	$src       = $languages[0];
 	$dest      = $languages[1];
-	$done      = 0;
-	while ( $done < 3 ) {
-		try {
-			$translationFrengly = FrenglyService::translate( $text, $src, $dest );
-		} catch ( \Exception $e ) {
-			$translationFrengly = $exception;
-			//try again after 3,5 sec
-			usleep( 3005000 );
-			$done ++;
-			continue;
-		}
-		$done = 3;
-	}
 	$done = 0;
 	while ( $done < 3 ) {
 		try {
@@ -67,17 +54,15 @@ if ( isset( $_POST['language-dir'] ) && isset( $_POST['text'] ) ) {
 if ( $translationGoogle[0] === '' ) {
 	$translationGoogle[0] = $defaultTranslation;
 }
-if ( $translationFrengly === '' ) {
-	$translationFrengly = $defaultTranslation;
-}
 ?>
 <div id="container">
-	<button id="language-pl_de" <?php echo $language === 'pl_de' ? '' : 'style="display:none;"' ?> value="pl_de"><?php echo $pl_de ?></button>
-	<button id="language-de_pl" <?php echo $language === 'de_pl' ? '' : 'style="display:none;"' ?> value="de_pl"><?php echo $de_pl ?></button>
+	<td><button id="language-pl_de" <?php echo $language === 'pl_de' ? '' : 'style="display:none;"' ?> value="pl_de"><?php echo $pl_de ?></button></td>
+	<td><button id="language-de_pl" <?php echo $language === 'de_pl' ? '' : 'style="display:none;"' ?> value="de_pl"><?php echo $de_pl ?></button></td>
 	<form method="post" action="http://<?php echo $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"] ?>">
 		<input type="hidden" id="language-dir" name="language-dir" value="<?php echo $language ?>" />
-		<textarea id="text" name="text" rows="2"><?php echo $text ?></textarea>
-		<div id="keyboardPL">
+		<p id="text" title="Tekst do tłumaczenia"><?php echo $text ?></p>
+		<input type="hidden" id="textInput" name="text" value="" />
+		<div id="keyboardPL" <?php echo $language === 'pl_de' ? '' : 'style="display:none;"' ?> >
 			<span class="helper">PL: </span>
 			<button value="ą">ą</button>
 			<button value="ć">ć</button>
@@ -89,24 +74,17 @@ if ( $translationFrengly === '' ) {
 			<button value="ź">ź</button>
 			<button value="ż">ż</button>
 		</div>
-		<div id="keyboardDE">
+		<div id="keyboardDE" <?php echo $language === 'de_pl' ? '' : 'style="display:none;"' ?>>
 			<span class="helper">DE: </span>
 			<button value="ä">ä</button>
 			<button value="ö">ö</button>
 			<button value="ß">ß</button>
 			<button value="ü">ü</button>
 		</div>
-		<section class="column">
-			<p class="helper">Google:</p>
-			<p id="translationGoogle">
-				<?php echo $translationGoogle[0] ?>
-				<?php echo isset( $translationGoogle[1] ) == true ? ' / ' . $translationGoogle[1] : '' ?>
-			</p>
-		</section>
-		<section class="column">
-			<p class="helper">Freengly:</p>
-			<p id="translationFreengly"><?php echo $translationFrengly ?></p>
-		</section>
+		<p id="translationGoogle">
+			<?php echo $translationGoogle[0] ?>
+			<?php echo isset( $translationGoogle[1] ) == true ? ' / ' . $translationGoogle[1] : '' ?>
+		</p>
 		<div class="clearfix"></div>
 		<button name="translate" id="translate" value="translate"><?php echo $translate ?></button>
 	</form>
--- js/main.js
+++ js/main.js
@@ -1,33 +1,85 @@
+if(!String.prototype.trim) {
+	String.prototype.trim = function () {
+		return this.replace(/^\s+|\s+$/g,'');
+	};
+}
 domready(function () {
-  //change language direction
-  var changeActiveLanguage = function(event) {
-    event.target.style['display'] = 'none';
-    switch(event.target.id) {
-        case "language-pl_de":
-            document.getElementById("language-de_pl").style['display'] = 'block';
-            document.getElementById('language-dir').value = document.getElementById("language-de_pl").value;
-            break;
-        case "language-de_pl":
-            document.getElementById("language-pl_de").style['display'] = 'block';
-            document.getElementById('language-dir').value = document.getElementById("language-pl_de").value;
-            break;
-    }
-  };
-  document.getElementById("language-pl_de").addEventListener("click", changeActiveLanguage, false);
-  document.getElementById("language-de_pl").addEventListener("click", changeActiveLanguage, false);
-  //keyboard
-  var insertDiacritic = function(event) {
-    event.preventDefault();
-    var letter = event.target.value;
-    document.getElementById("text").focus();
-    document.getElementById("text").value += letter;
-  };
-  var PLkeys = document.querySelectorAll("#keyboardPL > button");
-  for(var i = 0; i < PLkeys.length; i++) {
-		PLkeys[i].addEventListener("click", insertDiacritic, false);
-  }
-	var DEkeys = document.querySelectorAll("#keyboardDE > button");
-  for(var i = 0; i < DEkeys.length; i++) {
-		DEkeys[i].addEventListener("click", insertDiacritic, false);
-  }
+	if (document.getElementById("text").innerText.trim() === '') {
+		document.getElementById("text").innerHTML = '<span class="placeholder">' + document.getElementById("text").title + '</span>';
+	}
+	var changeActiveLanguage = function (event) {
+		switch (event.target.id) {
+			case "language-pl_de":
+				document.getElementById("language-de_pl").style['display'] = 'block';
+				document.getElementById("keyboardPL").style['display'] = 'none';
+				document.getElementById("keyboardDE").style['display'] = 'block';
+				document.getElementById('language-dir').value = document.getElementById("language-de_pl").value;
+				break;
+			case "language-de_pl":
+				document.getElementById("language-pl_de").style['display'] = 'block';
+				document.getElementById("keyboardPL").style['display'] = 'block';
+				document.getElementById("keyboardDE").style['display'] = 'none';
+				document.getElementById('language-dir').value = document.getElementById("language-pl_de").value;
+				break;
+		}
+		event.target.style['display'] = 'none';
+	};
+	document.getElementById("language-pl_de").addEventListener("click", changeActiveLanguage, false);
+	document.getElementById("language-de_pl").addEventListener("click", changeActiveLanguage, false);
+	var insertDiacritic = function (event) {
+		event.preventDefault();
+		var letter = event.target.value;
+		insertLetter(letter);
+	};
+	var deleteLastLetter = function () {
+		var now = document.getElementById("text").innerHTML;
+		document.getElementById("text").innerHTML = now.substring(0, now.length-1);
+		if (document.getElementById("text").innerHTML.trim() === '') {
+			document.getElementById("text").innerHTML = '<span class="placeholder">' + document.getElementById("text").title + '</span>';
+		}
+	};
+	var insertLetter = function (letter) {
+		if (document.querySelector("#text>.placeholder") != null) {
+			document.getElementById("text").innerHTML = '';
+		}
+		document.getElementById("text").innerHTML += letter;
+	};
+	var submitForm = function (event) {
+		if (document.querySelector("#text>.placeholder") != null) {
+			document.getElementById("text").innerHTML = '';
+		}
+		document.getElementById('textInput').value = document.getElementById('text').innerText;
+		document.getElementsByTagName("form")[0].submit();
+	};
+	addEventListener("keydown", function (event) {
+		var pressedKey = parseInt(event.keyCode, 10),
+				pressedKeyCode = pressedKey;
+		if (pressedKey === 8) {
+			event.preventDefault();
+			pressedKey = 'backspace';
+			onKeyboardKeyPress(pressedKey, pressedKeyCode);
+		}
+	});
+	addEventListener("keypress", function (event) {
+		event.preventDefault();
+		var pressedKey = parseInt(event.which, 10),
+				pressedKeyCode = pressedKey;
+		pressedKey = String.fromCharCode(pressedKey);
+		onKeyboardKeyPress(pressedKey, pressedKeyCode);
+	});
+	var onKeyboardKeyPress = function (key, code) {
+		if (key === "backspace") {
+			deleteLastLetter();
+		} else if ( key.length === 1
+								&& ( (code >= 63 && code <= 90) || (code >= 97 && code <= 122) || (code >= 48 && code <= 59)
+											|| (code >= 44 && code <= 59) || code === 33 || code === 32 )
+							) {
+			insertLetter(key);
+		}
+	};
+	var keys = document.querySelectorAll("#keyboardPL > button, #keyboardDE > button");
+	for (var i = 0; i < keys.length; i++) {
+		keys[i].addEventListener("click", insertDiacritic, false);
+	}
+	document.getElementById('translate').addEventListener('click', submitForm, false);
 });
